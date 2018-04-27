dbee978b379695672a66afa61540e68381625145
Mikey O'Toole <mikey@mjco.uk>
2018-04-27 13:41:22 +0100

    Civix Update

--- genderselfidentify.php
+++ genderselfidentify.php
@@ -1,9 +1,10 @@
 <?php
 
 require_once 'genderselfidentify.civix.php';
+use CRM_Genderselfidentify_ExtensionUtil as E;
 
 /**
- * Implements hook_civicrm_config
+ * Implements hook_civicrm_config().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
  */
@@ -12,9 +13,7 @@ function genderselfidentify_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu
- *
- * @param $files array(string)
+ * Implements hook_civicrm_xmlMenu().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
  */
@@ -23,17 +22,25 @@ function genderselfidentify_civicrm_xmlMenu(&$files) {
 }
 
 /**
- * Implements hook_civicrm_install
+ * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
  */
 function genderselfidentify_civicrm_install() {
   _genderselfidentify_civix_civicrm_install();
-  _genderselfidentify_add_other_option();
 }
 
 /**
- * Implements hook_civicrm_uninstall
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
+ */
+function genderselfidentify_civicrm_postInstall() {
+  _genderselfidentify_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
  */
@@ -42,63 +49,25 @@ function genderselfidentify_civicrm_uninstall() {
 }
 
 /**
- * Implements hook_civicrm_enable
+ * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function genderselfidentify_civicrm_enable() {
   _genderselfidentify_civix_civicrm_enable();
-  _genderselfidentify_add_other_option();
-
-  $group = civicrm_api3('CustomGroup', 'get', array(
-    'name' => 'Genderselfidentify',
-  ));
-  if (!empty($group['id'])) {
-    // CustomGroup 'create' is broken for update
-    civicrm_api3('CustomGroup', 'update', array(
-      'id' => $group['id'],
-      'is_reserved' => 1,
-      'is_active' => 1,
-    ));
-  }
 }
 
 /**
- * Implements hook_civicrm_disable
+ * Implements hook_civicrm_disable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
  */
 function genderselfidentify_civicrm_disable() {
   _genderselfidentify_civix_civicrm_disable();
-  try {
-    $group = civicrm_api3('CustomGroup', 'get', array(
-      'name' => 'Genderselfidentify',
-    ));
-    if (!empty($group['id'])) {
-      // CustomGroup 'create' is broken for update
-      civicrm_api3('CustomGroup', 'update', array(
-        'id' => $group['id'],
-        'is_reserved' => 0,
-        'is_active' => 0,
-      ));
-    }
-    civicrm_api3('OptionValue', 'create', array(
-      'id' => CRM_Genderselfidentify_BAO_Gender::otherOption('id'),
-      'is_reserved' => 0,
-    ));
-  }
-  // If custom data doesn't exist, ignore
-  catch (API_Exception $e) {}
 }
 
 /**
- * Implements hook_civicrm_upgrade
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
+ * Implements hook_civicrm_upgrade().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
  */
@@ -107,7 +76,7 @@ function genderselfidentify_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) 
 }
 
 /**
- * Implements hook_civicrm_managed
+ * Implements hook_civicrm_managed().
  *
  * Generate a list of entities to create/deactivate/delete when this module
  * is installed, disabled, uninstalled.
@@ -119,7 +88,34 @@ function genderselfidentify_civicrm_managed(&$entities) {
 }
 
 /**
- * Implements hook_civicrm_alterSettingsFolders
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function genderselfidentify_civicrm_caseTypes(&$caseTypes) {
+  _genderselfidentify_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+ */
+function genderselfidentify_civicrm_angularModules(&$angularModules) {
+  _genderselfidentify_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
  */
@@ -128,183 +124,40 @@ function genderselfidentify_civicrm_alterSettingsFolders(&$metaDataFolders = NUL
 }
 
 /**
- * Implements hook_civicrm_apiWrappers
- * 
- * @param array $wrappers
- * @param array $apiRequest
- */
-function genderselfidentify_civicrm_apiWrappers(&$wrappers, $apiRequest) {
-  if (strtolower($apiRequest['entity']) == 'contact') {
-    $wrappers[] = new CRM_Genderselfidentify_ContactAPIWrapper();
-  }
-}
-
-/**
- * Implements hook_civicrm_buildForm
- * 
- * @param string $formName
- * @param CRM_Core_Form $form
- */
-function genderselfidentify_civicrm_buildForm($formName, &$form) {
-  if (in_array($formName, array('CRM_Contact_Form_Contact', 'CRM_Contact_Form_Inline_Demographics', 'CRM_Profile_Form_Edit'))
-  && $form->elementExists('gender_id')) {
-    $form->removeElement('gender_id');
-    $form->addElement('text', 'gender_id', ts('Gender'));
-    if (!empty($form->_contactId)) {
-      $form->setDefaults(array(
-        'gender_id' => CRM_Genderselfidentify_BAO_Gender::get($form->_contactId),
-      ));
-    }
-    // Hide custom field from contact edit screen since it is not editable
-    if ($formName == 'CRM_Contact_Form_Contact') {
-      CRM_Core_Resources::singleton()
-        ->addStyle('#Genderselfidentify.crm-custom-accordion {display: none;}', 99, 'html-header');
-    }
-  }
-}
-
-/**
- * Implements hook_civicrm_pre
- * 
- * @param string $op
- * @param string $objectName
- * @param int|null $id
- * @param array $params
- */
-function genderselfidentify_civicrm_pre($op, $objectName, $id, &$params) {
-  if ($objectName == 'Individual' && in_array($op, array('create', 'edit'))) {
-    // $params['version'] indicates this is an api request, which we've already handled with api_v3_GenderselfidentifyAPIWrapper
-    if (isset($params['gender_id']) && empty($params['version'])) {
-      $input = trim($params['gender_id']);
-      $params['gender_id'] = CRM_Genderselfidentify_BAO_Gender::match($input);
-
-      // Can't just set `$params['custom_x'] = $input` because that would be too easy
-      // For contact create
-      $params['custom_' . CRM_Genderselfidentify_BAO_Gender::getCustomFieldId() . '_-1'] = $input;
-      // For contact inline-edit
-      $params += array('custom' => array());
-      CRM_Core_BAO_CustomField::formatCustomField(CRM_Genderselfidentify_BAO_Gender::getCustomFieldId(), $params['custom'],
-        $input, 'Individual', NULL, $id, FALSE, FALSE, TRUE
-      );
-    }
-  }
-}
-
-/**
- * Implements hook_civicrm_pageRun
- * 
- * @param CRM_Core_Page $page
- */
-function genderselfidentify_civicrm_pageRun(&$page) {
-  $pageClass = get_class($page);
-
-  // For contact summary view
-  if (in_array($pageClass, array('CRM_Contact_Page_View_Summary', 'CRM_Contact_Page_Inline_Demographics'))) {
-    $cid = $page->get_template_vars('id');
-    if ($cid) {
-      $page->assign('gender_display', htmlspecialchars(CRM_Genderselfidentify_BAO_Gender::get($cid)));
-    }
-    // Hide custom field from contact summary since its value is incorporated in the demographics pane
-    CRM_Core_Resources::singleton()
-      ->addStyle('.customFieldGroup.Genderselfidentify {display: none;}', 99, 'html-header');
-  }
-
-  // For profile listings
-  elseif ($pageClass == 'CRM_Profile_Page_Listings') {
-    $genderRow = NULL;
-    $columnHeaders = $page->get_template_vars('columnHeaders');
-    if ($columnHeaders) {
-      foreach ($columnHeaders as $num => $col) {
-        if (CRM_Utils_Array::value('field_name', $col) === 'gender_id') {
-          $genderRow = $num;
-        }
-      }
-    }
-    if ($genderRow) {
-      $rows = $page->get_template_vars('rows');
-      if ($rows) {
-        $other = CRM_Genderselfidentify_BAO_Gender::otherOption('label');
-        foreach ($rows as &$row) {
-          if ($row[$genderRow] == $other) {
-            // Dammit, no cid in row, have to parse it from the view link in the last column
-            preg_match('#[&?;]id=(\d+)#', $row[count($row)-1], $matches);
-            if (!empty($matches[1])) {
-              $row[$genderRow] = htmlspecialchars(CRM_Genderselfidentify_BAO_Gender::get($matches[1]));
-            }
-          }
-        }
-        $page->assign('rows', $rows);
-      }
-    }
-  }
-
-  // For profile view
-  elseif (in_array($pageClass, array('CRM_Profile_Page_View', 'CRM_Profile_Page_Dynamic'))) {
-    $profileFields = $page->get_template_vars('profileFields');
-    $row = $page->get_template_vars('row');
-    foreach ($profileFields as $key => &$field) {
-      if ($key == 'gender_id') {
-        $row[$field['label']] = $field['value'] = htmlspecialchars(CRM_Genderselfidentify_BAO_Gender::get($page->get_template_vars('cid')));
-        $page->assign('row', $row);
-        $page->assign('profileFields', $profileFields);
-        break;
-      }
-    }
-  }
-}
-
-/**
- * Implements hook_civicrm_searchColumns
- * 
- * @param string $objectName
- * @param array $headers
- * @param array $rows
- * @param $selector
- */
-function genderselfidentify_civicrm_searchColumns($objectName, &$headers, &$rows, &$selector) {
-  if (strtolower($objectName) == 'contact') {
-    $other = CRM_Genderselfidentify_BAO_Gender::otherOption('label');
-    foreach ($rows as &$row) {
-      if (isset($row['gender_id']) && $row['gender_id'] == $other && !empty($row['contact_id'])) {
-        $row['gender_id'] = htmlspecialchars(CRM_Genderselfidentify_BAO_Gender::get($row['contact_id']));
-      }
-    }
-  }
-}
-
-/**
- * Add "Other" gender option if it doesn't exist
- * Ensure it is enabled and reserved if it already exists
+ * Implements hook_civicrm_entityTypes().
  *
- * @throws \CiviCRM_API3_Exception
+ * Declare entity types provided by this module.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
  */
-function _genderselfidentify_add_other_option() {
-  $options = civicrm_api3('OptionValue', 'get', array('option_group_id' => 'gender'));
-  $maxValue = 1;
-  foreach ($options['values'] as $lastOption) {
-    if ($lastOption['name'] === 'Other') {
-      // Make sure it is enabled and reserved
-      if (empty($lastOption['is_active']) || empty($lastOption['is_reserved'])) {
-        civicrm_api3('OptionValue', 'create', array(
-          'id' => $lastOption['id'],
-          'is_active' => 1,
-          'is_reserved' => 1,
-        ));
-      }
-      return;
-    }
-    if ($lastOption['value'] > $maxValue) {
-      $maxValue = $lastOption['value'];
-    }
-  }
-  // We're still here, so "Other" option needs to be added
-  civicrm_api3('OptionValue', 'create', array(
-    'option_group_id' => 'gender',
-    'name' => 'Other',
-    'label' => ts('Other'),
-    'value' => $maxValue + 1,
-    'weight' => $lastOption['weight'] + 1,
-    'is_active' => 1,
-    'is_reserved' => 1,
-  ));
+function genderselfidentify_civicrm_entityTypes(&$entityTypes) {
+  _genderselfidentify_civix_civicrm_entityTypes($entityTypes);
 }
+
+// --- Functions below this ship commented out. Uncomment as required. ---
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
+ *
+function genderselfidentify_civicrm_preProcess($formName, &$form) {
+
+} // */
+
+/**
+ * Implements hook_civicrm_navigationMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_navigationMenu
+ *
+function genderselfidentify_civicrm_navigationMenu(&$menu) {
+  _genderselfidentify_civix_insert_navigation_menu($menu, 'Mailings', array(
+    'label' => E::ts('New subliminal message'),
+    'name' => 'mailing_subliminal_message',
+    'url' => 'civicrm/mailing/subliminal',
+    'permission' => 'access CiviMail',
+    'operator' => 'OR',
+    'separator' => 0,
+  ));
+  _genderselfidentify_civix_navigationMenu($menu);
+} // */
